d0574caaabb4971e3a6c5ea7c768aaf9754a3c2d
[Michael] <michael@lewiscommunications.com>
2015-10-08 18:59:42 -0500

    through jquery #4

--- JQuery/j.js
+++ JQuery/j.js
@@ -1,7 +1,33 @@
-$(document).ready( function(){
-	$("#alert_button").click( function(){
+$(document).ready(function () {
+	$("#alert_button").click(function () {
 		alert('I am an alert!');
 	});
+
+	$('#submit_button').click(function () {
+		var x = $("#textarea").val();
+		alert(x);
+	});
+
+	$('.forum').hover(
+		function () {
+			var $this = $(this);
+			$this.data('bgcolor', $this.css('background-color')).css('background-color', '#380606');
+		},
+		function () {
+			var $this = $(this);
+			$this.css('background-color', $this.data('bgcolor'));
+		}
+		);
+
+	$('p').click(function () {
+		$(this).css(
+			'color', 'rgb(' + (Math.floor(Math.random() * 256)) + ',' +
+			(Math.floor(Math.random() * 256)) + ',' +
+			(Math.floor(Math.random() * 256)) + ')'
+		, 500);
+	});
+
+
 });
 
 
